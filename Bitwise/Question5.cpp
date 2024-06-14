05f3dd6224ee765f017ad41490f4c11ee2594640
Sudhir Kumar <sudhir9122kumar@gmail.com>
2024-06-14 12:10:06 +0530

    15 June 2024

---
+++ Bitwise/Question5.cpp
@@ -0,0 +1,20 @@
+//  Write a C program to clear nth bit of a number.
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    int bit_pos;
+    printf("Enter bit position: ");
+    scanf("%d",&bit_pos);
+    if(bit_pos<0 || bit_pos> sizeof(int)*8){
+        printf("Invalid input");
+        return 0;
+    }
+    int n = 1;
+    n = n << bit_pos-1;
+    n = ~n;
+    num = (num & n);
+    printf("Number after clearing bit: %d",num);
+    return 0;
+}
